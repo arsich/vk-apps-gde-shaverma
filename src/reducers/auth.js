30c6e95832696c892575e67947d035766d8e4dfe
Arseny Vasilev <mail@arsich.ru>
2018-08-16 21:49:17 +0300

    Fix profile update after rating added

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,6 +1,8 @@
 import {NEW_AUTH_INFO, GET_VK_USER_INFO_SUCCESS, 
     GET_MY_PROFILE, GET_MY_PROFILE_SUCCESS, GET_MY_PROFILE_FAILED, HIDE_INTRO} from '../actions/auth'
 
+import {ADD_RATING_SUCCESS, DELETE_RATING_SUCCESS} from '../actions/places'
+
 export default function authReducer(state = {hasAuth: false, introShown: false}, action = {}) {
     switch (action.type) {
         case NEW_AUTH_INFO:
@@ -34,6 +36,12 @@ export default function authReducer(state = {hasAuth: false, introShown: false},
             return {
                 ...state,
                 introShown: true
+            }
+        case ADD_RATING_SUCCESS:
+        case DELETE_RATING_SUCCESS:
+            return {
+                ...state,
+                profileInfo: null
             }
         default:
             return state;
